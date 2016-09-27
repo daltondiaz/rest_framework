dc9dfa3ff55800b0763c6d1e69cb24098490c646
Dalton <dalton.dias@ymail.com>
2016-09-26 21:31:20 -0300

    refactoring to use ViewSets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -9,28 +9,34 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+	"""
+	This viewset automatically provides 'list', 'create', 'retrieve',
+	'update' and 'destroy' actions.
+
+	Additionally we also provide an extra 'highlight' action.
+	"""
 
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly,)
-	
+	permission_class = (permissions.IsAuthenticatedOrReadOnly,
+						IsOwnerOrReadOnly,)
+
+	@detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+	def highlight(self, request, *args,**kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
+
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly,
-							IsOwnerOrReadOnly,)
-
-class UserList(generics.ListAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	This viewset automatically provides 'list' and 'details' actions.
+	"""
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
@@ -41,10 +47,4 @@ def api_root(request,format=None):
 		'snippets' : reverse('snippet-list', request=request, format=format)
 	})
 
-class SnippetHighlight(generics.GenericAPIView):
-	queryset = Snippet.objects.all()
-	renderer_classes = (renderers.StaticHTMLRenderer,)
 
-	def get(self, request, *args, **kwargs):
-		snippet = self.get_object()
-		return Response(snippet.highlighted)
